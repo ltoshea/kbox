f7ba518de1ac1f0e91c1b84f0723d5c983aa14ff
lo0446 <lo0446@my.bristol.ac.uk>
2014-04-11 20:52:11 +0100

    v

---
+++ reconstructPose.m
@@ -0,0 +1,20 @@
+%function reconstructBeaks(A, Xm,U)
+%U = eigenvectors (EV)
+function reconstructPose(A,Xm,U)
+for i = 1:size(A, 1)
+    %testbeak = A(i,:)';
+    testpose = A(:,i);
+    a = U'*(testpose-Xm); %% Get projection coefficients
+    y = U*a+Xm;
+    
+    close all
+    hold on
+    plot3(testpose(1:20),testpose(21:40),testpose(41:60),'-gx')
+    plot3(y(1:20),y(21:40),y(41:60),'-r');
+    %set(gca,'XLim',[0 1000], 'YLim',[200 200]);
+    axis equal;
+    r = norm(y-testpose);
+    disp(sprintf('MSRE batch: %.4f', r));
+    pause
+end
+end
