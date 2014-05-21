f7d252c669fe9686caed5bc18e97b83d22a56cad
lo0446 <lo0446@my.bristol.ac.uk>
2014-05-21 16:55:55 +0100

    NEARLY THERE!

--- gensvclabels - Copy.m
+++
@@ -1,9 +0,0 @@
-function [labels] = gensvclabels(Y)
-
-Y(1:10) = 1;
-Y(11:20) = 2;
-Y(21:30) = 3;
-Y(31:40) = 4;
-Y(41:50) = 5;
-Y(51:end) = 6;
-labels = Y;
--- randforest - Copy.m
+++
@@ -1,2 +0,0 @@
-function [ rfdata ] = ranforest( Z2 )
-
